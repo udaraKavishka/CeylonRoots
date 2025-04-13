7e421ae41916c0efe27f1e025f48e1c62d46a726
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-14 01:56:50 +0530

    feat: update image configuration to use remote patterns for Unsplash

--- ceylonroots/next.config.ts
+++ ceylonroots/next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    formats: ["image/avif", "image/webp"], 
   },
 };
 
