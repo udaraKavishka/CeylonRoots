d85a2652c0af913e627590a318ab9524a31cb0d0
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-18 02:53:37 +0530

    feat: add Destination entity for travel component management with attractions

---
+++ backend/src/main/java/com/example/backend/model/Destination.java
@@ -0,0 +1,14 @@
+package com.example.backend.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Destination extends TravelComponent{
+	private String attractions;
+}
