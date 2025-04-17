2d5fee39420f7a59fea2f291132425294b1003b9
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-18 02:52:41 +0530

    feat: update User entity to include firstName, lastName, phoneNumber, and remove unused fields

--- backend/src/main/java/com/example/backend/model/User.java
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -1,12 +1,13 @@
 package com.example.backend.model;
 
-import jakarta.persistence.Column;
+
+import java.util.List;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,14 +21,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(unique = true, nullable = false)
+    private String firstName;
+    private String lastName;
     private String email;
+    private String password;  
+    private String phoneNumber;
+    private String role;  
 
-    private String password;
-    private String fullName;
+    @OneToMany(mappedBy = "user")
+    private List<Booking> bookings;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
+
 
 }
