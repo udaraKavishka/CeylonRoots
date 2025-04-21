f0226a19497cd8c19f5390589a6fdc681784b02b
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-21 17:00:17 +0530

    feat: create MessageResponse DTO for standardized response messages

---
+++ backend/src/main/java/com/example/backend/dto/MessageResponse.java
@@ -0,0 +1,26 @@
+package com.example.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+	private String message;
+
+	public MessageResponse(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
