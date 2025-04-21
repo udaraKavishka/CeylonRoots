45c993cbb6f3edf1bacc66718d69162cde2f90c2
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-21 16:58:40 +0530

    refactor: enhance User class to implement UserDetails and streamline role management

--- backend/src/main/java/com/example/backend/model/User.java
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -1,130 +1,157 @@
 package com.example.backend.model;
 
-
-import java.util.List;
-import java.util.Set;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import lombok.Builder;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
+@Table(name = "users")
 @Data
-@NoArgsConstructor
-@Builder
-public class User {
+
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String firstName;
     private String lastName;
     
-    @Column(unique = true)
+@Column(unique = true)
     private String email;
     
-    
-    private String password;  
+    private String password;
     private String phoneNumber;
-
-    @OneToMany(mappedBy = "user")
+    
+@OneToMany(mappedBy = "user")
     private List<Booking> bookings;
     
-//    @Enumerated(EnumType.STRING)
-//    private Role role;
+@ElementCollection(fetch = FetchType.EAGER)
+@Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
     
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Enumerated(EnumType.STRING)
-    private Set<Role> roles;
+@Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+    
+@Override
+    public String getUsername() {
+        return email;
+    }
+    
+@Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    
+@Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    
+@Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+@Override
+    public boolean isEnabled() {
+        return true;
+    }
 
-	public User(Long id, String firstName, String lastName, String email, String password, String phoneNumber,
-			List<Booking> bookings, Set<Role> roles) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.password = password;
-		this.phoneNumber = phoneNumber;
-		this.bookings = bookings;
-		this.roles = roles;
-	}
+public User(Long id, String firstName, String lastName, String email, String password, String phoneNumber,
+		List<Booking> bookings, Set<Role> roles) {
+	super();
+	this.id = id;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	this.password = password;
+	this.phoneNumber = phoneNumber;
+	this.bookings = bookings;
+	this.roles = roles;
+}
 
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+public Long getId() {
+	return id;
+}
 
-	public String getFirstName() {
-		return firstName;
-	}
+public void setId(Long id) {
+	this.id = id;
+}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+public String getFirstName() {
+	return firstName;
+}
 
-	public String getLastName() {
-		return lastName;
-	}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+public String getLastName() {
+	return lastName;
+}
 
-	public String getEmail() {
-		return email;
-	}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+public String getEmail() {
+	return email;
+}
 
-	public String getPassword() {
-		return password;
-	}
+public void setEmail(String email) {
+	this.email = email;
+}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+public String getPassword() {
+	return password;
+}
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+public void setPassword(String password) {
+	this.password = password;
+}
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+public String getPhoneNumber() {
+	return phoneNumber;
+}
 
-	public List<Booking> getBookings() {
-		return bookings;
-	}
+public void setPhoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
+}
 
-	public void setBookings(List<Booking> bookings) {
-		this.bookings = bookings;
-	}
+public List<Booking> getBookings() {
+	return bookings;
+}
 
-	public Set<Role> getRoles() {
-		return roles;
-	}
+public void setBookings(List<Booking> bookings) {
+	this.bookings = bookings;
+}
 
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
+public Set<Role> getRoles() {
+	return roles;
+}
 
-	
+public void setRoles(Set<Role> roles) {
+	this.roles = roles;
+}
+
+
+
     
 
 
