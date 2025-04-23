4ff98aa85ffc59e1347ea077e2eecb5a13fc0653
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-24 01:20:06 +0530

    refactor: update UserRepository to change ID type to Long and modify user retrieval methods

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.example.backend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.backend.model.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-	Optional<User> findByEmail(String email);
-    Boolean existsByEmail(String email);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
