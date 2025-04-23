8faf8f01f2db129e59e2f7cfd2a3c6745e4bb7f5
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-24 01:18:59 +0530

    refactor: update AuthController and related DTOs for improved authentication handling

--- backend/src/main/java/com/example/backend/dto/MessageResponse.java
+++ backend/src/main/java/com/example/backend/dto/MessageResponse.java
@@ -1,22 +1,22 @@
-package com.example.backend.dto;
-
-public class MessageResponse {
-
-    private String message;
-
-    public MessageResponse(String message) {
-        this.message = message;
-    }
-
-    public MessageResponse() {
-    }
-
-    
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-}
+//package com.example.backend.dto;
+//
+//public class MessageResponse {
+//
+//    private String message;
+//
+//    public MessageResponse(String message) {
+//        this.message = message;
+//    }
+//
+//    public MessageResponse() {
+//    }
+//
+//    
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+//}
