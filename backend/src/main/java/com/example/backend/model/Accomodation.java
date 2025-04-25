430343a459af27f568582225c424b34000af599f
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-25 22:25:49 +0530

    refactor: update Java version to 21 and remove Lombok @Data annotation from model classes

--- backend/src/main/java/com/example/backend/model/Accomodation.java
+++ backend/src/main/java/com/example/backend/model/Accomodation.java
@@ -2,11 +2,9 @@ package com.example.backend.model;
 
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Accomodation extends TravelComponent {
