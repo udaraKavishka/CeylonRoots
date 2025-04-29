483d2302e002f489c6cc47384984eeb7d4ce31df
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-29 23:25:19 +0530

    feat: enhance TravelComponent with inheritance and discriminator column

--- backend/src/main/java/com/example/backend/model/TravelComponent.java
+++ backend/src/main/java/com/example/backend/model/TravelComponent.java
@@ -1,19 +1,24 @@
 package com.example.backend.model;
 
+import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "component_type")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TravelComponent {
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
@@ -23,6 +28,5 @@ public class TravelComponent {
     private double price;
     private double lat;
     private double lng;
-    private String type;
-    private int duration;  
+    private int duration;
 }
