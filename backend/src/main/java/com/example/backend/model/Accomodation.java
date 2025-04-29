497e8e1e47caaf6644027d7a7c90bfad188b84af
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-29 23:25:56 +0530

    fix: add discriminator value to Accomodation entity and adjust formatting

--- backend/src/main/java/com/example/backend/model/Accomodation.java
+++ backend/src/main/java/com/example/backend/model/Accomodation.java
@@ -1,13 +1,15 @@
 package com.example.backend.model;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
+@DiscriminatorValue("ACCOMMODATION")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Accomodation extends TravelComponent {
-	private String amenities;
+    private String amenities;
     private int rating;
 }
