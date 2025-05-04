88efcbf1800312c975375523bfd5e5bbfdb0018b
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-04 23:56:13 +0530

    feat: add complete constructor and getter/setter methods for DestinationDetails class

--- backend/src/main/java/com/example/backend/model/DestinationDetails.java
+++ backend/src/main/java/com/example/backend/model/DestinationDetails.java
@@ -36,4 +36,129 @@ public class DestinationDetails {
 
     @ElementCollection
     private List<String> gallery;
+
+	public DestinationDetails(Long id, String name, String description, String region, String image,
+			String topAttraction, String bestTimeToVisit, String recommendedDuration, String culturalTips,
+			List<String> attractions, double lat, double lng, List<String> gallery) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.region = region;
+		this.image = image;
+		this.topAttraction = topAttraction;
+		this.bestTimeToVisit = bestTimeToVisit;
+		this.recommendedDuration = recommendedDuration;
+		this.culturalTips = culturalTips;
+		this.attractions = attractions;
+		this.lat = lat;
+		this.lng = lng;
+		this.gallery = gallery;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getTopAttraction() {
+		return topAttraction;
+	}
+
+	public void setTopAttraction(String topAttraction) {
+		this.topAttraction = topAttraction;
+	}
+
+	public String getBestTimeToVisit() {
+		return bestTimeToVisit;
+	}
+
+	public void setBestTimeToVisit(String bestTimeToVisit) {
+		this.bestTimeToVisit = bestTimeToVisit;
+	}
+
+	public String getRecommendedDuration() {
+		return recommendedDuration;
+	}
+
+	public void setRecommendedDuration(String recommendedDuration) {
+		this.recommendedDuration = recommendedDuration;
+	}
+
+	public String getCulturalTips() {
+		return culturalTips;
+	}
+
+	public void setCulturalTips(String culturalTips) {
+		this.culturalTips = culturalTips;
+	}
+
+	public List<String> getAttractions() {
+		return attractions;
+	}
+
+	public void setAttractions(List<String> attractions) {
+		this.attractions = attractions;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLng() {
+		return lng;
+	}
+
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+
+	public List<String> getGallery() {
+		return gallery;
+	}
+
+	public void setGallery(List<String> gallery) {
+		this.gallery = gallery;
+	}
+    
+    
 }
