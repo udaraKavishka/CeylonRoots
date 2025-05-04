236fe576bdb9f39b3d780746359facec013c3fe2
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-04 23:56:26 +0530

    feat: add complete constructor and getter/setter methods for Transport class

--- backend/src/main/java/com/example/backend/model/Transport.java
+++ backend/src/main/java/com/example/backend/model/Transport.java
@@ -17,4 +17,23 @@ public class Transport extends TravelComponent {
     private double departureLng;
     private double arrivalLat;
     private double arrivalLng;
+    
+    
+	public Transport(Long id, String name, String description, String location, String image, double price, double lat,
+			double lng, int duration, String mode, String departureLocation, String arrivalLocation,
+			double departureLat, double departureLng, double arrivalLat, double arrivalLng) {
+		super();
+		this.mode = mode;
+		this.departureLocation = departureLocation;
+		this.arrivalLocation = arrivalLocation;
+		this.departureLat = departureLat;
+		this.departureLng = departureLng;
+		this.arrivalLat = arrivalLat;
+		this.arrivalLng = arrivalLng;
+	}
+    
+    
+    
+    
+    
 }
