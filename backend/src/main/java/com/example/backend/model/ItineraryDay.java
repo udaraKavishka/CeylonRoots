039b52b0189316ac6eb2ac15e84427c0907be863
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-04 23:56:19 +0530

    feat: add complete constructor and getter/setter methods for ItineraryDay class

--- backend/src/main/java/com/example/backend/model/ItineraryDay.java
+++ backend/src/main/java/com/example/backend/model/ItineraryDay.java
@@ -27,4 +27,74 @@ public class ItineraryDay {
 
     @ElementCollection
     private List<String> activities;
+
+	public ItineraryDay(Long id, String title, String location, String description, String accommodation, String meals,
+			List<String> activities) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.location = location;
+		this.description = description;
+		this.accommodation = accommodation;
+		this.meals = meals;
+		this.activities = activities;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAccommodation() {
+		return accommodation;
+	}
+
+	public void setAccommodation(String accommodation) {
+		this.accommodation = accommodation;
+	}
+
+	public String getMeals() {
+		return meals;
+	}
+
+	public void setMeals(String meals) {
+		this.meals = meals;
+	}
+
+	public List<String> getActivities() {
+		return activities;
+	}
+
+	public void setActivities(List<String> activities) {
+		this.activities = activities;
+	}
+    
+    
 }
