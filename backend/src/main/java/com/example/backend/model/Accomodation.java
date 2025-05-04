c78577bcf8e6cc47fe3f1e9278deee5152cf66cb
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-04 23:55:43 +0530

    style: format Accomodation class for improved readability

--- backend/src/main/java/com/example/backend/model/Accomodation.java
+++ backend/src/main/java/com/example/backend/model/Accomodation.java
@@ -10,6 +10,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Accomodation extends TravelComponent {
-    private String amenities;
+	
+	private String amenities;
     private int rating;
+    
+    public String getAmenities() {
+		return amenities;
+	}
+	public void setAmenities(String amenities) {
+		this.amenities = amenities;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public Accomodation(String amenities, int rating) {
+		super();
+		this.amenities = amenities;
+		this.rating = rating;
+	}
+	
+	
+	
 }
+
