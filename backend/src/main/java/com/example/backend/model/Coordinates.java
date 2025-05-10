78036b554e03a5741067a799496eb35f29fb95d7
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-11 01:38:23 +0530

    feat: add Coordinates class to represent geographical coordinates with latitude and longitude

---
+++ backend/src/main/java/com/example/backend/model/Coordinates.java
@@ -0,0 +1,15 @@
+package com.example.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coordinates {
+	private Double lat;
+    private Double lng;
+}
