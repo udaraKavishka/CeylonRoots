e800438e74ca4e11bd755a398f3726f4fb817cd8
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-11 01:39:07 +0530

    refactor:ItineraryDay class to extend BaseEntity

--- backend/src/main/java/com/example/backend/model/ItineraryDay.java
+++ backend/src/main/java/com/example/backend/model/ItineraryDay.java
@@ -1,132 +1,30 @@
 package com.example.backend.model;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ItineraryDay {
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class ItineraryDay extends BaseEntity {
     private String title;
     private String location;
     private String description;
-    private String accommodation;
     private String meals;
 
+    @ManyToOne
+    @JoinColumn(name = "accommodation_id")
+    private Accommodation accommodation;
 
     @ManyToMany
-    @JoinTable(
-        name = "day_activities",
-        joinColumns = @JoinColumn(name = "day_id"),
-        inverseJoinColumns = @JoinColumn(name = "activity_id")
-    )
-    private List<Activity> activities ;
-    
-    
+    @JoinTable(name = "itinerary_activities",
+        joinColumns = @JoinColumn(name = "itinerary_day_id"),
+        inverseJoinColumns = @JoinColumn(name = "activity_id"))
+    private Set<Activity> activities = new HashSet<>();
+
     @ManyToOne
-    @JoinColumn(name = "travel_package_id", nullable = false)
-    @JsonBackReference
+    @JoinColumn(name = "travel_package_id")
     private TravelPackage travelPackage;
-
-	public ItineraryDay(Long id, String title, String location, String description, String accommodation, String meals) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.location = location;
-		this.description = description;
-		this.accommodation = accommodation;
-		this.meals = meals;
-
-	}
-
-	public ItineraryDay() {
-		super();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getAccommodation() {
-		return accommodation;
-	}
-
-	public void setAccommodation(String accommodation) {
-		this.accommodation = accommodation;
-	}
-
-	public String getMeals() {
-		return meals;
-	}
-
-	public void setMeals(String meals) {
-		this.meals = meals;
-	}
-
-	public List<Activity> getActivities() {
-		return activities;
-	}
-
-	public void setActivities(List<Activity> activities) {
-		this.activities = activities;
-	}
-
-	public TravelPackage getTravelPackage() {
-		return travelPackage;
-	}
-
-	public void setTravelPackage(TravelPackage travelPackage) {
-		this.travelPackage = travelPackage;
-	}
-
-
-
-
-    
-    
 }
