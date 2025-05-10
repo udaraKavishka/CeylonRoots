526977552927291ad081826de61e7e49f339f57b
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-11 01:38:02 +0530

    feat: add BaseEntity class for common entity attributes and timestamp management

---
+++ backend/src/main/java/com/example/backend/model/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.backend.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
