cbe234ac0e8ccdbeb169dd2b427151a62720422d
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-12 01:55:05 +0530

    refactor: update Accommodation class with constructor and getter/setter methods for amenities and rating

--- backend/src/main/java/com/example/backend/model/Accommodation.java
+++ backend/src/main/java/com/example/backend/model/Accommodation.java
@@ -1,5 +1,7 @@
 package com.example.backend.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,6 +20,32 @@ public class Accommodation extends TravelComponent {
     private Set<String> amenities = new HashSet<>();
     
     private Double rating;
+
+	public Accommodation(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, String name, String description,
+			String location, String imageUrl, BigDecimal price, Coordinates coordinates, Integer duration,
+			Set<String> tags, Set<String> amenities, Double rating) {
+		super(id, createdAt, updatedAt, name, description, location, imageUrl, price, coordinates, duration, tags);
+		this.amenities = amenities;
+		this.rating = rating;
+	}
+
+	public Set<String> getAmenities() {
+		return amenities;
+	}
+
+	public void setAmenities(Set<String> amenities) {
+		this.amenities = amenities;
+	}
+
+	public Double getRating() {
+		return rating;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+    
+    
 
     
 }
