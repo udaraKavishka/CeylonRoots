126262603a0e4e5bc79672d5d39239d2078c32c4
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-12 01:56:43 +0530

    feat: add attractions field with getter and setter methods to Destination class

--- backend/src/main/java/com/example/backend/model/Destination.java
+++ backend/src/main/java/com/example/backend/model/Destination.java
@@ -1,6 +1,8 @@
 package com.example.backend.model;
 
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,4 +20,21 @@ public class Destination extends TravelComponent {
 	@Convert(converter = StringListConverter.class)
     @Column(columnDefinition = "JSON")
     private List<String> attractions;
+
+	public Destination(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, String name, String description,
+			String location, String imageUrl, BigDecimal price, Coordinates coordinates, Integer duration,
+			Set<String> tags, List<String> attractions) {
+		super(id, createdAt, updatedAt, name, description, location, imageUrl, price, coordinates, duration, tags);
+		this.attractions = attractions;
+	}
+
+	public List<String> getAttractions() {
+		return attractions;
+	}
+
+	public void setAttractions(List<String> attractions) {
+		this.attractions = attractions;
+	}
+	
+	
 }
