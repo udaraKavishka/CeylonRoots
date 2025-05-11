2078eef5866bfce2439426a9133f3d1ce47f2adf
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-12 01:52:45 +0530

    refactor: add constructors to BlogPost class for improved initialization

--- backend/src/main/java/com/example/backend/model/BlogPost.java
+++ backend/src/main/java/com/example/backend/model/BlogPost.java
@@ -1,6 +1,7 @@
 package com.example.backend.model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -15,6 +16,27 @@ import lombok.*;
 @Entity
 @Data
 public class BlogPost extends BaseEntity {
+	public BlogPost(Long id, LocalDateTime createdAt, LocalDateTime updatedAt) {
+		super(id, createdAt, updatedAt);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BlogPost(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, String title, String excerpt,
+			String content, String imageUrl, LocalDate postDate, String author, String category, List<String> tags,
+			List<BlogComment> comments, List<Long> relatedPostIds) {
+		super(id, createdAt, updatedAt);
+		this.title = title;
+		this.excerpt = excerpt;
+		this.content = content;
+		this.imageUrl = imageUrl;
+		this.postDate = postDate;
+		this.author = author;
+		this.category = category;
+		this.tags = tags;
+		this.comments = comments;
+		this.relatedPostIds = relatedPostIds;
+	}
+
 	private String title;
     private String excerpt;
     
