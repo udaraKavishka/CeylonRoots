45ae4d4c081a769ef3b8a46afac7ecdf0e0d7e2e
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-12 01:53:11 +0530

    refactor: remove redundant constructor and accessors in BaseEntity class

--- backend/src/main/java/com/example/backend/model/BaseEntity.java
+++ backend/src/main/java/com/example/backend/model/BaseEntity.java
@@ -20,4 +20,37 @@ public class BaseEntity {
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
+	public BaseEntity(Long id, LocalDateTime createdAt, LocalDateTime updatedAt) {
+		super();
+		this.id = id;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+    
+    
 }
