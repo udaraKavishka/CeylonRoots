069801ab6f18f7d69f6074a94d0ffcbc7036e152
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-12 01:54:46 +0530

    refactor: simplify Coordinates class by adding constructor and getter/setter methods

--- backend/src/main/java/com/example/backend/model/Coordinates.java
+++ backend/src/main/java/com/example/backend/model/Coordinates.java
@@ -12,4 +12,23 @@ import lombok.NoArgsConstructor;
 public class Coordinates {
 	private Double lat;
     private Double lng;
+	public Coordinates(Double lat, Double lng) {
+		super();
+		this.lat = lat;
+		this.lng = lng;
+	}
+	public Double getLat() {
+		return lat;
+	}
+	public void setLat(Double lat) {
+		this.lat = lat;
+	}
+	public Double getLng() {
+		return lng;
+	}
+	public void setLng(Double lng) {
+		this.lng = lng;
+	}
+    
+    
 }
