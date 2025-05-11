9885084e1f45668de7f78ad3aa1f5f925d63fe91
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-12 00:28:58 +0530

    refactor: replace list for attractions in Destination class

--- backend/src/main/java/com/example/backend/model/Destination.java
+++ backend/src/main/java/com/example/backend/model/Destination.java
@@ -2,6 +2,7 @@ package com.example.backend.model;
 
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.*;
@@ -12,19 +13,7 @@ import lombok.*;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Destination extends TravelComponent {
-    @ElementCollection
-    @CollectionTable(name = "destination_attractions")
-    private Set<String> attractions = new HashSet<>();
-
-    @OneToOne(mappedBy = "destination", cascade = CascadeType.ALL)
-    private DestinationDetails details;
-
-    @ManyToMany(mappedBy = "destinations")
-    private Set<TravelPackage> travelPackages = new HashSet<>();
-
-    @ManyToMany
-    @JoinTable(name = "destination_gallery",
-        joinColumns = @JoinColumn(name = "destination_id"),
-        inverseJoinColumns = @JoinColumn(name = "gallery_item_id"))
-    private Set<GalleryItem> galleryItems = new HashSet<>();
+	@Convert(converter = StringListConverter.class)
+    @Column(columnDefinition = "JSON")
+    private List<String> attractions;
 }
