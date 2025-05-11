9b19d2d562d367e25e6586fa300aa0ffd0ddb7ac
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-12 01:53:53 +0530

    refactor: remove unused imports and improve code cleanliness in Activity class

--- backend/src/main/java/com/example/backend/model/Activity.java
+++ backend/src/main/java/com/example/backend/model/Activity.java
@@ -1,6 +1,8 @@
 package com.example.backend.model;
 
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,6 +18,21 @@ public class Activity extends TravelComponent {
 
     @Enumerated(EnumType.STRING)
     private Difficulty difficulty;
+
+	public Activity(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, String name, String description,
+			String location, String imageUrl, BigDecimal price, Coordinates coordinates, Integer duration,
+			Set<String> tags, Difficulty difficulty) {
+		super(id, createdAt, updatedAt, name, description, location, imageUrl, price, coordinates, duration, tags);
+		this.difficulty = difficulty;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
 
 
 
