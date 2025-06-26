0de5106fc2b1878f6f3d0b2eb47c64f521d30656
Udara Nalawansa <udarakavishka13@gmail.com>
2025-06-27 01:05:30 +0530

    feat: update package image paths and add new travel packages with detailed itineraries and pricing

--- ceylonroots/app/data/packageImageMap.ts
+++ ceylonroots/app/data/packageImageMap.ts
@@ -1,70 +1,74 @@
 // Map of package IDs to local image paths
 export const packageImages: Record<string, string> = {
-    "1": "/images/home/img1.jpg",
-    "2": "/images/home/img1.jpg",
-    "3": "/images/home/img1.jpg",
-    "4": "/images/home/img1.jpg",
-    "5": "/images/home/img1.jpg",
-    "6": "/images/home/img1.jpg",
-    "7": "/images/home/img1.jpg",
-    "8": "/images/home/img1.jpg",
-    "9": "/images/home/img1.jpg",
-    "10": "/images/home/img1.jpg",
-    "11": "/images/home/img1.jpg", 
-    "12": "/images/home/img1.jpg", 
-    "13": "/images/home/img1.jpg", 
-    "14": "/images/home/img1.jpg", 
-    "15": "/images/home/img1.jpg", 
-    "16": "/images/home/img1.jpg", 
-    "17": "/images/home/img1.jpg", 
+  "1": "https://ceylonrootsbucket.s3.eu-north-1.amazonaws.com/ella.jpg",
+  "2": "/images/home/img1.jpg",
+  "3": "/images/home/img1.jpg",
+  "4": "/images/home/img1.jpg",
+  "5": "/images/home/img1.jpg",
+  "6": "/images/home/img1.jpg",
+  "7": "/images/home/img1.jpg",
+  "8": "/images/home/img1.jpg",
+  "9": "/images/home/img1.jpg",
+  "10": "/images/home/img1.jpg",
+  "11": "/images/home/img1.jpg",
+  "12": "/images/home/img1.jpg",
+  "13": "/images/home/img1.jpg",
+  "14": "/images/home/img1.jpg",
+  "15": "/images/home/img1.jpg",
+  "16": "/images/home/img1.jpg",
+  "17": "/images/home/img1.jpg",
 };
 
 // Map of package IDs to gallery images
 export const packageGalleries: Record<string, string[]> = {
-    "17": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],
-    "8": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],
-    "9": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],
-    "10": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],
-    "11": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],
-    "12": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],"13": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],"14": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],"15": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],"16": [
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg",
-        "/images/home/img1.jpg"
-    ],
+  "17": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
+  "8": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
+  "9": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
+  "10": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
+  "11": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
+  "12": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
+  "13": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
+  "14": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
+  "15": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
+  "16": [
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+    "/images/home/img1.jpg",
+  ],
 };
